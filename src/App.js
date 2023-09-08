c5b57143eea264451579b1a288bcbde192ca395c
Av-code80 <dev.avehghasemian@gmail.com>
2023-09-08 13:06:26 +0200

    feat: :sparkles:

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Form from "./scenes/form";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Invoices from "./scenes/invoices/Index";
-//import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar/Index";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -32,12 +32,13 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
                <Route path="/invoices" element={<Invoices />} />
                <Route path="/form" element={<Form />} />
+               <Route path="/calendar" element={<Calendar />} />
               {/*
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
+              />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
