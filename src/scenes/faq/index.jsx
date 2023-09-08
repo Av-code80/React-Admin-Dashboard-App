6e56deea25e45f29593d52c854e1c579a9560ba5
Av-code80 <dev.avehghasemian@gmail.com>
2023-09-08 14:08:48 +0200

    refactor: :construction: FAQ in progress by Accordion Summary

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -9,7 +9,20 @@ import { tokens } from "../../theme";
 
 
 const FAQ = () => {
-  return <div>Faq</div>;
+     const theme = useTheme();
+     const colors = tokens(theme.palette.mode);
+
+
+  return <Box m="20px">
+    <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+    <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon/>}>
+        <Typography color={colors.greenAccent[500]} variant="h5">
+        An Important question
+        </Typography>
+        </AccordionSummary>
+    </Accordion>
+  </Box>
 };
 
 export default FAQ
