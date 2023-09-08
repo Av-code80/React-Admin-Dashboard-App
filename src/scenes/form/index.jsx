1ed9c23936d8fca49fd0006d9ce273acb4857407
Av-code80 <dev.avehghasemian@gmail.com>
2023-09-08 09:50:38 +0200

    feat: :sparkles: Implement Form submit including error handling for new user creation

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -1,9 +1,8 @@
-import { Box, TextField, Typography, useTheme } from "@mui/material";
+import { Box, Button, TextField, Typography, useTheme } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-import useMediaQuery from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
-
 
 const initialValues = {
   firstName: "",
@@ -21,7 +20,10 @@ const userSchema = yup.object().shape({
   firstName: yup.string().required("required"),
   LastName: yup.string().required("required"),
   email: yup.string().email("Invalid email").required("required"),
-  contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
   address1: yup.string().required("required"),
   address2: yup.string().required("required"),
 });
@@ -32,8 +34,6 @@ const Form = () => {
   const handleFormSubmit = (values) => {
     console.log(values);
   };
-
-  
 
   return (
     <Box m="20px">
@@ -61,20 +61,88 @@ const Form = () => {
               }}
             >
               <TextField
-               fullWidth
-               varian="filled"
-               type="text"
-               label="First Name"
-               onBlur={handleBlur}
-               onChange={handleChange}
-               value={values.firstName}
-               name="firstName"
-               error={!!touched.firstName && !!errors.firstName}
-               helperText={touched.firstName && errors.firstName}
-               sx={{gridColumn: "span 2"}}
-               >
-
-               </TextField>
+                fullWidth
+                varian="filled"
+                type="text"
+                label="First Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.firstName}
+                name="firstName"
+                error={!!touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                varian="filled"
+                type="text"
+                label="Last Name"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.lastName}
+                name="LastName"
+                error={!!touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                fullWidth
+                varian="filled"
+                type="text"
+                label="Email"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+                name="email"
+                error={!!touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                varian="filled"
+                type="text"
+                label="Contact Number"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contact}
+                name="contact"
+                error={!!touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                varian="filled"
+                type="text"
+                label="Address 1"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address1}
+                name="address1"
+                error={!!touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}
+                sx={{ gridColumn: "span 4" }}
+              />
+              <TextField
+                fullWidth
+                varian="filled"
+                type="text"
+                label="Address 2"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.address2}
+                name="address2"
+                error={!!touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
+                sx={{ gridColumn: "span 4" }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="end" mt="20px">
+              <Button type="submit" color="secondary" variant="contained">
+                Create New user
+              </Button>
             </Box>
           </form>
         )}
